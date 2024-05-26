f6266cb74bbe67d9de2dc820935d66519b509860
OFFICIAL-KING <63652867+OFFICIAL-KING@users.noreply.github.com>
2024-05-26 21:43:34 +0530

    Add files via upload

--- Setup.sh
+++ Setup.sh
@@ -1,8 +1,23 @@
 #!/bin/bash/
 
 termux-setup-storage
-apt update && apt upgrade -y && nmap -y && nano -y && python -y && wget -y && nodejs -y && curl -y && ruby -y && openssh -y && python -y 
-pkg install python -y && python3 -y && git -y && wget -y && curl -y && python2 -y && gh -y
+apt update && apt upgrade -y
+apt install nmap -y
+apt install nano -y
+apt install python -y
+apt install wget -y
+apt install nodejs -y
+apt install curl -y
+apt install ruby -y
+apt install openssh -y
+pkg install python -y
+pkg install python3 -y
+pkg install git -y
+pkg install wget -y
+pkg install curl -y
+pkg install python2 -y
+apt install python -y 
+pkg install gh -y
 echo 'clear && echo "\nPlease \033[1;32mPress Enter \033[0mto Continue\n" && gh cs ssh' > gh && chmod +x gh;
 
 RED='\033[1;31m'  # Bold Red
