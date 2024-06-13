e9b2ba1573f22392e478a7b3aac57527e6e3a6ce
OFFICIAL-KING <63652867+OFFICIAL-KING@users.noreply.github.com>
2024-06-14 02:58:42 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -24,4 +24,4 @@ RED='\033[1;31m'  # Bold Red
 GREEN='\033[1;32m'  # Bold green
 YELLOW='\033[1;33m'  # Bold yellow
 NOCLR='\033[0m'  # No color
-gh auth login -s cs && clear && echo -e "\n${YELLOW}Setup Done\n${RED}Key ${NOCLR}has been ${GREEN}Logged in Successfully\n${NOCLR}Next Time Use this ${YELLOW}./gh ${NOCLR}Command to RUN \n\n${NOCLR}Please ${GREEN}Press Enter ${NOCLR}to Continue\n" && sleep 5 && clear && gh cs ssh
+gh auth login -s cs && clear && echo -e "\n${YELLOW}Setup Done\n${RED}Key ${NOCLR}has been ${GREEN}Logged in Successfully\n\n${NOCLR}Please ${GREEN}Press Enter ${NOCLR}to Continue\n" && sleep 5 && clear && gh cs ssh
